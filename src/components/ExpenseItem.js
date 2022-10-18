ff9371eb6477fc578886b953390daaab353e1a84
Lawrence <locaulker@gmail.com>
2022-10-18 06:06:31 -0400

    end of L37

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,15 @@
 import React from "react"
-import ExpenseDate from "./ExpenseDate"
 import "./ExpenseItem.css"
-import Card from "./Card"
 
 function ExpenseItem(props) {
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </Card>
+    </div>
   )
 }
 
